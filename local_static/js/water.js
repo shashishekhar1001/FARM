9ff23ea60e94d9d64186f22db720165831f2803e
Shashishekhar Hasabnis <shashishekhar1001@gmail.com>
2021-03-18 13:48:01 +0530

    Fetch Fields & Water using axion onn Water.html Page

--- local_static/js/water.js
+++ local_static/js/water.js
@@ -5,6 +5,48 @@ const FormApp = {
             field_name: "HI",    
         }
     },
+    mounted() {
+        axios({
+            method : "GET",
+            url:"/api/waters/", 
+            headers: {'X-CSRFTOKEN': '{{ csrf_token }}', 'Content-Type': 'application/json'},
+        }).then(response => {
+            console.log(response.data);
+            this.fields=response.data;
+            console.log(this.fields);
+        }).catch(err => {
+            console.log(err);
+            Swal.fire({
+                icon: 'error',
+                title: 'Failed Loading Data...',
+                text: err,
+            })
+        });
+
+        axios({
+            method : "GET",
+            url:"/api/fields/", 
+            headers: {'X-CSRFTOKEN': '{{ csrf_token }}', 'Content-Type': 'application/json'},
+        }).then(response => {
+            console.log(response.data);
+            this.fields=response.data;
+            console.log(this.fields);
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Your fields & waters have been loaded',
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }).catch(err => {
+            console.log(err);
+            Swal.fire({
+                icon: 'error',
+                title: 'Failed Loading Data...',
+                text: err,
+            })
+        });
+    },
     methods: {
     }
         
